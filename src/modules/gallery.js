7deabb7dfb35fbd2c62d08308dcd04e20a7b10d3
Gabegar <Gagarcia190@gmail.com>
2022-12-16 15:27:44 -0500

    updates: code for removal of an img no lngr used

--- src/modules/gallery.js
+++ src/modules/gallery.js
@@ -2,14 +2,8 @@ export default function gallery() {
     const tabContentDiv = document.querySelector(".tab_content");
     const directoryGallery = "images/gallery/image";
 
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 4; i++) {
         const galleryImg = document.createElement("img");
-
-        if (i === 0) {
-            galleryImg.src = `${directoryGallery}${i}.jpg`;
-            galleryImg.alt = "Gallery item";
-            galleryImg.classList.add("gallery_img");
-        }
 
         if (i === 1) {
             galleryImg.src = `${directoryGallery}${i}.jpg`;
